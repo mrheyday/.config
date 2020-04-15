f731c344b23a0010fe80bef2de0b3091c869d6df
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-04-15 21:11:29 +0300

    Use histdb_top for autosuggestions & make it case insensitive

--- zsh/autosuggest.zsh
+++ zsh/autosuggest.zsh
@@ -1,13 +1,16 @@
-export ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=( end-of-line )
-export ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=( forward-char emacs-forward-word )
-export ZSH_AUTOSUGGEST_STRATEGY=( histdb_top_here completion )
-_zsh_autosuggest_strategy_histdb_top_here() {
+export ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=( end-of-line vi-end-of-line vi-add-eol )
+export ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(
+  forward-char forward-word emacs-forward-word down-line
+)
+export ZSH_AUTOSUGGEST_STRATEGY=( histdb_top completion )
+# `COLLATE UTF8_GENERAL_CI` makes it case insensitive.
+_zsh_autosuggest_strategy_histdb_top() {
     local query="select commands.argv from
 history left join commands on history.command_id = commands.rowid
 left join places on history.place_id = places.rowid
-where places.dir LIKE '$(sql_escape $PWD)%'
-and commands.argv LIKE '$(sql_escape $1)%'
-group by commands.argv order by count(*) desc limit 1"
+where commands.argv COLLATE UTF8_GENERAL_CI LIKE '$(sql_escape $1)%'
+group by commands.argv
+order by places.dir != '$(sql_escape $PWD)', count(*) desc limit 1"
     suggestion=$(_histdb_query "$query")
 }
 
