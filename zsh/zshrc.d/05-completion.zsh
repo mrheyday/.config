77258825ac46cc5c99d36dba8b77a92257536c27
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-10-07 21:58:35 +0300

    Lazy load Python-related completions
    ...instead of caching them.

--- zsh/zshrc.d/05-completion.zsh
+++ zsh/zshrc.d/05-completion.zsh
@@ -8,10 +8,18 @@ znap source marlonrichert/zsh-autocomplete
 # Auto-installed by Brew, but far worse than the one supplied by Zsh
 rm -f $HOMEBREW_PREFIX/share/zsh/site-functions/_git{,.zwc}
 
-# Include absolute path for `znap eval` cache invalidation.
-znap eval pyenv-init ${${:-=pyenv}:P}' init - --no-rehash'
 
-# Include Python version as comment, for `znap eval` cache invalidation.
-znap eval    pip-completion "pip completion --zsh             # $PYENV_VERSION"
-znap eval   pipx-completion "register-python-argcomplete pipx # $PYENV_VERSION"
-znap eval pipenv-completion "pipenv --completion              # $PYENV_VERSION"
+# Set up lazy loading for Python-related completions:
+
+znap function _pyenv pyenv              'eval "$( pyenv init - --no-rehash )"'
+compctl -K    _pyenv pyenv
+
+znap function _pip_completion pip       'eval "$( pip completion --zsh )"'
+compctl -K    _pip_completion pip
+
+znap function _python_argcomplete pipx  'eval "$( register-python-argcomplete pipx  )"'
+complete -o nospace -o default -o bashdefault \
+           -F _python_argcomplete pipx
+
+znap function _pipenv pipenv            'eval "$( pipenv --completion )"'
+compdef       _pipenv pipenv
